6ccfc84cca7843f1f2813defe712c6a510a7a193
Patrick Xavier <123217203+Patrickxaviercury@users.noreply.github.com>
2023-07-22 23:10:28 -0300

    calc.js

--- cnode2/calc.js
+++ cnode2/calc.js
@@ -2,7 +2,7 @@ var somafunc = require("./soma");
 var subfunc = require("./sub");
 var multifunc = require("./multi");
 var multidiv = require("./div");
-
+ 
 console.log(somafunc(1,2));
 console.log(subfunc(10,5));
 console.log(multifunc(100,10));
